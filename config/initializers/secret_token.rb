89ef1293e939af1a58f10fcffd74c5bb6fee1cf7
Andreas H. Koenig <ahkoenig17@googlemail.com>
2012-10-11 22:57:49 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlitzReport::Application.config.secret_token = '4187b1c3b6ca68000e77e8f4f63df79a2ca366cee337ab3a22f6b81cbb9e46a5c1be503c72dbecd9ecc9638964eadee4c056d4f19c1bb5d77be4ed71460c7a84'
